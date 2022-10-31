396e70ee5cabcb5e261af467c1f37e8a26432b1d
dev-ansh-r <rdevanshshukla26@gmail.com>
2022-10-31 14:45:59 +0530

    Serail monitor and baud rate

---
+++ Serial_monitor/Serialprint.ino/Serialprint.ino.ino
@@ -0,0 +1,21 @@
+//Assigning values to variables
+
+float radius=1; 
+int delayTime = 500;
+float area;
+String String1="Area of the circle with radius ";
+float pi=3.14;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  area = pi*radius*radius;
+  Serial.print(String1);
+  Serial.print(radius);
+  Serial.print(" is ");
+  Serial.println(area);
+  radius=radius+1;
+  delay(delayTime);
+}
