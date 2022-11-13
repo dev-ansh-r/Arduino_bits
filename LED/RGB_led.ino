1fec7bb7aa649db772ecf6b29dbf0d03dd7cdf1e
dev-ansh-r <rdevanshshukla26@gmail.com>
2022-11-13 11:10:58 +0530

    RGB_LED and also Aqua color

---
+++ LED/RGB_led.ino
@@ -0,0 +1,54 @@
+int redPin=8;
+int greenPin=9;
+int bluePin=10;
+String myColor;
+String msg="What color LED do you want ?";
+int wT=500;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(redPin,OUTPUT);
+pinMode(greenPin,OUTPUT);
+pinMode(bluePin,OUTPUT);
+}
+
+void loop() {
+Serial.println(msg);
+while(Serial.available()==0){
+  
+}
+myColor=Serial.readString();
+
+if(myColor=="red" || myColor=="RED"){
+  digitalWrite(redPin,HIGH);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(bluePin,LOW);
+  delay(wT);
+}
+if(myColor=="green" || myColor=="GREEN"){
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,HIGH);
+  digitalWrite(bluePin,LOW);
+  delay(wT);
+}
+
+if(myColor=="blue" || myColor=="BLUE"){
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(bluePin,HIGH);
+  delay(wT);
+}
+if(myColor=="off" || myColor=="OFF"){
+  digitalWrite(redPin,LOW);
+  digitalWrite(greenPin,LOW);
+  digitalWrite(bluePin,LOW);
+  delay(wT);
+}
+if(myColor=="aqua" || myColor=="AQUA"){
+  analogWrite(redPin,LOW);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,80);
+  delay(wT);
+}
+}
